b971e30ddee0ea0dd4bfb8eb6ba67b7ec649e122
Frenk44 <f.haulussy@hotmail.com>
2022-11-10 15:30:33 +0100

    pcap SUPPORT
    store PCAP data

--- src/main/java/nl/fah/common/Utils.java
+++ src/main/java/nl/fah/common/Utils.java
@@ -1,14 +1,22 @@
 package nl.fah.common;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import nl.fah.logger.DataLogger;
+import org.pcap4j.core.*;
+import org.pcap4j.packet.Packet;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.zip.Adler32;
+import java.util.zip.CheckedInputStream;
+import java.util.zip.CheckedOutputStream;
 
 public class Utils {
 
     // convert InputStream to String
-    public static String getStringFromInputStream(InputStream iss) {
+    public static String getStringFromInputStream(InputStream iss) throws IOException {
 
         BufferedReader br = null;
         StringBuilder sb = new StringBuilder();
@@ -22,19 +30,37 @@ public class Utils {
             }
 
         } catch (IOException e) {
-            e.printStackTrace();
+            throw e;
         } finally {
             if (br != null) {
                 try {
                     br.close();
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    throw e;
                 }
             }
         }
 
         return sb.toString();
 
+    }
+    
+    public static void dumpPCAP(Packet[] packets, String fileName) throws PcapNativeException, NotOpenException, IOException {
+        PcapHandle handle = null;
+        PcapDumper dumper;
+
+        dumper = handle.dumpOpen(fileName);
+
+        for (Packet packet:packets)
+        {
+            try {
+
+                dumper.dump(packet, handle.getTimestamp());
+            } catch (NotOpenException e) {
+                e.printStackTrace();
+            }
+        }
+        dumper.close();
     }
 
 }
